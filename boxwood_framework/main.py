19dfd139deb7d572118cdcb6060ecd72bdf7c06e
PAVELPE <pavelpe@amdocs.com>
2021-04-23 14:44:21 +0300

    Lesson02

--- boxwood_framework/main.py
+++ boxwood_framework/main.py
@@ -1,4 +1,5 @@
 import quopri
+from requests import GetRequests, PostRequests
 
 
 class PageNotFound404:
@@ -24,6 +25,20 @@ class Framework:
         # приведение к нижнему регистру
         path = path.lower()
 
+        request = {}
+        # Получаем все данные запроса
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            request['data'] = data
+            print(f'Нам пришёл post-запрос: {Framework.decode_value(data)}')
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = request_params
+            print(f'Нам пришли GET-параметры: {request_params}')
+
         # находим нужный контроллер
         # отработка паттерна page controller
         if path in self.routes_lst:
@@ -41,3 +56,11 @@ class Framework:
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
 
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = quopri.decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
